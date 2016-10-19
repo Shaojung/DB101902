cd676e63183d928383d57f00ba655e188de7e9b7
Shaojung <abc@abc.com>
2016-10-19 20:38:39 +0800

    Initial commit

---
+++ app/src/main/java/tw/com/pcschool/db101902/MainActivity.java
@@ -0,0 +1,28 @@
+package tw.com.pcschool.db101902;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    NotificationManager nm;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    }
+
+    public void click1(View v)
+    {
+        Notification.Builder builer = new Notification.Builder(MainActivity.this);
+        builer.setContentTitle("Test1")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentText("這是 Notification 測試");
+        Notification notification = builer.build();
+        nm.notify(123, notification);
+
+    }
+}
